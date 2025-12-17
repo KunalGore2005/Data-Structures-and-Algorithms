78e536888c1af690ff166f32f493cc28ed446f9f
Kunal Gore <gorekunal665@gmail.com>
2025-12-17 23:03:08 +0530

    CodeForces Solution

---
+++ CodeForces/677A-Vanya_and_Fence.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n,h;
+    cin>>n>>h;
+    int ans=0;
+    for(int i=0;i<n;i++)
+    {
+        int a;
+        cin>>a;
+        if(a>h)
+            ans+=2;
+        else
+            ans+=1;
+    }
+    cout<<ans;
+    return 0;
+}
