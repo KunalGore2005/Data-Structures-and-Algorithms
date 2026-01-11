b6de684bad50c5f69728dba023b6fe531e2dc704
Kunal Gore <gorekunal665@gmail.com>
2026-01-11 22:34:53 +0530

    CodeForces Submission

---
+++ CodeForces/230A-Dragons.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+int main()
+{
+    int s,n;
+    cin>>s>>n;
+    vector<pair<int,int>>dragons;
+    for(int i=0;i<n;i++){
+        int x,y;
+        cin>>x>>y;
+        dragons.push_back(make_pair(x,y));
+    }
+    sort(dragons.begin(),dragons.end(),[](const pair<int,int>&a, const pair<int,int>&b){
+        return a.first<b.first;
+    });
+    for(int i=0;i<n;i++){
+        if(s>dragons[i].first){
+            s+=dragons[i].second;
+        }
+        else{
+            cout<<"NO"<<endl;
+            return 0;
+        }
+    }
+    cout<<"YES"<<endl;
+    return 0;
+}
