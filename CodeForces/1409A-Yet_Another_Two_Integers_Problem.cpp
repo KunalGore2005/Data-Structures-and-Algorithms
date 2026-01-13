3a438465f364dd747b2db0035f11da2684f7bb98
Kunal Gore <gorekunal665@gmail.com>
2026-01-13 21:11:01 +0530

    CodeForces Solution

--- CodeForces/1409A-Yet_Another_Two_Integers_Problem.cpp
+++ CodeForces/1409A-Yet_Another_Two_Integers_Problem.cpp
@@ -12,23 +12,7 @@ int main()
         int result=0;
         result+= diff / 10;
         diff%=10;
-        result+= diff / 9;
-        diff%=9;
-        result+= diff / 8;
-        diff%=8;
-        result+= diff / 7;
-        diff%=7;
-        result+= diff / 6;
-        diff%=6;
-        result+= diff / 5;
-        diff%=5;
-        result+= diff / 4;
-        diff%=4;
-        result+= diff / 3;
-        diff%=3;
-        result+= diff / 2;
-        diff%=2;
-        result+= diff;
+        if(diff)result++;
         cout<<result<<"\n";
     }
     return 0;
