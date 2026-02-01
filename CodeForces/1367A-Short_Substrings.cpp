c358a668a2c7bdc9adba657e17222d7ae7db41f2
Kunal Gore <gorekunal665@gmail.com>
2026-02-01 22:28:22 +0530

    CodeForces Submission

---
+++ CodeForces/1367A-Short_Substrings.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--){
+        string s;
+        cin>>s;
+        string res="";
+        res+=s[0];
+        for(int i=1;i<s.length()-1;i+=2){
+            res+=s[i];
+        }
+        res+=s[s.length()-1];
+        cout<<res<<endl;
+    }
+    return 0;
+}
