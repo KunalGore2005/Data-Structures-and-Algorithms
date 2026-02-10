a58ea58953ea8657fbbc109d39d42dd27e513948
Kunal Gore <gorekunal665@gmail.com>
2026-02-10 17:03:24 +0530

    CodeFores Submission

---
+++ CodeForces/1926A-Vlad_and_the_Best_of_Five.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--){
+        int a=0,b=0;
+        for(int i=0;i<5;i++){
+            char x;
+            cin>>x;
+            if(x=='A') a++;
+            else b++;
+        }
+        if(a>b) cout<<"A"<<endl;
+        else if(b>a) cout<<"B"<<endl;
+    }
+    return 0;
+}
