31547aa845eab299b3c9d60ce7ac28b3587c2d22
Kunal Gore <gorekunal665@gmail.com>
2026-02-12 23:43:06 +0530

    CodeForces Submission

---
+++ CodeForces/1772A-A+B.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--){
+        int a,c;
+        char b;
+        cin>>a>>b>>c;
+        cout<<a+c<<endl;
+    }
+    return 0;
+}
