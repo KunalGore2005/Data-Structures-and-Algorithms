0da283f7bb1a69f3725f1e27400c137e85390c15
Kunal Gore <gorekunal665@gmail.com>
2026-02-17 20:16:21 +0530

    CodeForces Submission

---
+++ CodeForces/2197A-FriendlyNumber.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int digit_sum(long long n) {
+    int sum = 0;
+    while (n > 0) {
+        sum += n % 10;
+        n /= 10;
+    }
+    return sum;
+}
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        long long x;
+        cin >> x;
+        int count = 0;
+        for (long long y = x + 1; y <= x + 81; y++) {
+            if (y - digit_sum(y) == x) {
+                count++;
+            }
+        }
+        cout << count << endl;
+    }
+    return 0;
+}
